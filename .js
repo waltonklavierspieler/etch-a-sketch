9919d0e74148dc86caea0fa74b7758d9714b8258
waltonklavierspieler <lottwa@gmail.com>
2023-04-10 14:06:45 -0600

    Starting project, linked .js sheet

--- .js
+++ .js
@@ -1,1 +1,16 @@
+/* logic
+create webpage of 16x16 grid of square divs
+  use javascript (for loop to generate new nodes, tracking rows and columns)
+  'it's best to put your grid squares inside another 'container' div'
+  use flexbox
+  create hover effect so grid divs change color
+  create a button asking for user input defining how many squares per side
+    of new grid (max 100) of the same size;
+
+'extra credit: Instead of just changing the color of a square from black
+ to white (for example), have each pass through with the mouse
+ change it to a completely random RGB value. Then try having each
+ pass just add another 10% of black to it so that only after 10
+  passes is the square completely black.
+  */
 
