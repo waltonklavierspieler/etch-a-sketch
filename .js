b1e48817fd190ebd6cdc31bbca0032d300362f44
waltonklavierspieler <lottwa@gmail.com>
2023-04-11 00:23:05 -0600

    .js now generated # of divs based on user input

--- .js
+++ .js
@@ -14,3 +14,26 @@ create webpage of 16x16 grid of square divs
   passes is the square completely black.
   */
 
+// get user-defined number of squares for the grid
+var squares = prompt("How many squares wide would you like your grid?");
+
+// test to make sure the number is within a reasonable range
+if (squares <=0 || squares >100) {
+  prompt("Number needs to be between 1 and 100.");
+}
+
+// normalize the size of the squares so total width is always 960px
+var squareWidth = 960 / squares;
+var squareHeight = 960 / squares;
+squareWidth = squareWidth.toString() + "px";
+squareHeight = squareHeight.toString() + "px";
+
+// generate the div elements
+for (var i = 1; i <= squares; i++) {
+	const div = document.createElement("div");
+	div.style.width = squareWidth;
+	div.style.height = squareHeight;
+	div.style.background = "white";
+	div.innerHtml = i.toString();
+	document.body.appendChild(div);
+}
