992959270a2dbb8a24f8b1cc65044b776531ec8f
waltonklavierspieler <lottwa@gmail.com>
2023-04-25 01:20:57 -0600

    Updated: grid and etching functional, with clear button and create grid button. A button for selecting colors has been created, but is not functioning properly yet.

--- .js
+++ .js
@@ -1,39 +1,117 @@
-/* logic
-create webpage of 16x16 grid of square divs
-  use javascript (for loop to generate new nodes, tracking rows and columns)
-  'it's best to put your grid squares inside another 'container' div'
-  use flexbox
-  create hover effect so grid divs change color
-  create a button asking for user input defining how many squares per side
-    of new grid (max 100) of the same size;
+//vvvvvvvvv Buttons and their related click events vvvvvvvvvv
+	//create a button for generating square
+	const gridButton = document.querySelector('button');
+	gridButton.addEventListener('click', createGrid);
 
-'extra credit: Instead of just changing the color of a square from black
- to white (for example), have each pass through with the mouse
- change it to a completely random RGB value. Then try having each
- pass just add another 10% of black to it so that only after 10
-  passes is the square completely black.
-  */
+	//a button to collapse the grid
+	const clearButton = document.getElementById('clearBoard');
+	clearButton.addEventListener('click', clearBoard);
 
-// get user-defined number of squares for the grid
-var squares = prompt("How many squares wide would you like your grid?");
+	//create shake button to revert all squares to white;
+	const shakeButton = document.getElementById('shake');
+	shakeButton.addEventListener('click', function () {
+		const shakeEffect = document.querySelectorAll('.row');
+		shakeEffect.forEach(function (node) {
+			node.style.background = "white";
+		});
+	});
 
-// test to make sure the number is within a reasonable range
-if (squares <=0 || squares >100) {
-  prompt("Number needs to be between 1 and 100.");
-}
+	//a button that returns a user-selected color
+	const colorSelect = document.getElementById('colorButton');
+	colorSelect.addEventListener('click', getColor);
+	const currentColor = getColor();
 
-// normalize the size of the squares so total width is always 960px
-var squareWidth = 960 / squares;
-var squareHeight = 960 / squares;
-squareWidth = squareWidth.toString() + "px";
-squareHeight = squareHeight.toString() + "px";
+	//helper function for selecting a color
+	function getColor() {
+		var colorSelector = 'black';
+		var colorList = ['black', 'red', 'purple', 'green', 'lime',
+						'yellow', 'navy', 'crimson'];
+		const buttonGenParent = document.getElementById('colorButton');
+		colorList.forEach(function (color) {
+			console.log('Color iteration test:' + color);
+			const button = document.createElement('button');
+			button.style.background = color;
+			buttonGenParent.appendChild(button);
+			button.classList.add('tempColors');
+			button.addEventListener('click', function() {
+				console.log('Current color: ' + currentColor);
+				console.log('clicked color: ' + button.style.background);
+				colorSelector = button.style.background;
+				const buttonRemoval = document.querySelectorAll('.tempColors');
+				console.log(buttonRemoval);
+				buttonRemoval.forEach(function (button) {
+					button.remove();
+				});
+			});
+		});
+		return colorSelector;
+	}
+	//helper function to get number input from user
+	function getNum () {
+		var num = prompt('Pick a number');
+		//account for cancel button
+		if (num === null) {
+			return
+		}
+		alert('You picked: ' + num);
+		//account for number out of range
+		if ((num < 0) || (num > 100)) {
+			alert('You need to pick a number between 1 - 100')
+			num = getNum();
+		}
+		return num;
+	}
 
-// generate the div elements
-for (var i = 1; i <= squares; i++) {
-	const div = document.createElement("div");
-	div.style.width = squareWidth;
-	div.style.height = squareHeight;
-	div.style.background = "white";
-	div.innerHtml = i.toString();
-	document.body.appendChild(div);
-}
+	//function to create grid
+	function createGrid(n) {
+		//clear grid first:
+		clearBoard();
+		n = getNum();
+		//for loop to generate n number of divs
+		const parent = document.querySelector('.container');
+		for (var i = 1; i <= n; i++) {
+			const div = document.createElement('div');
+			div.classList.add('column');
+			div.id = i.toString();
+			//div.innerHTML = i.toString();
+			div.style.border = '0.5px solid black';
+			parent.appendChild(div);
+			//console.log('New div id is:' + div.id);
+		}
+
+		const columnList = document.querySelectorAll('.column');
+		columnList.forEach(
+			function(node) {
+				for (var k = 1; k <= n; k++) {
+					const div = document.createElement('div');
+					div.classList.add('row');
+					div.id = k.toString();
+					//div.innerHTML = k.toString();
+					div.style.border = '0.5px solid black';
+					node.appendChild(div);
+					//console.log('New div id is:' + div.id);
+				}
+			}
+		);
+
+		//create on Hover effect to change color of each div;
+		const hoverEffect = document.querySelectorAll('.row');
+		hoverEffect.forEach(function (node) {
+			//console.log(node);
+			node.addEventListener('mouseover', function (node) {
+				node.toElement.style.background = currentColor;
+				//console.log(node.toElement.style);
+			}
+		);
+	});
+	}
+	//function to clear the board and destroy the grid of divs
+	function clearBoard () {
+		const columnList = document.querySelectorAll('.column');
+		//console.log('Removal attempt of nodeList: ' + columnList);
+		columnList.forEach(
+			function(node) {
+				node.remove();
+			}
+		);
+	}
